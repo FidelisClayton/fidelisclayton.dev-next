12c819571468e33d83abbb8806ed16a05647c7a3
Clayton Fidelis <clayton.fidelis@live.com>
2021-08-07 09:44:16 +0200

    fix: storybook

--- components/ArticleItem/ArticleItem.stories.tsx
+++ components/ArticleItem/ArticleItem.stories.tsx
@@ -5,4 +5,15 @@ export default {
   component: ArticleItem,
 }
 
-export const Default = () => <ArticleItem />
+export const Default = () => (
+  <ArticleItem
+    post={{
+      title: 'Hello World',
+      id: 'hello-world',
+      date: '2020-01-01T00:00:00Z',
+      description: 'Hello World.',
+      serie: 'Hello World',
+      episode: 1,
+    }}
+  />
+)
