e6ec03cd56789aaa6670df300c99eeaa411d030b
Clayton Fidelis <clayton.fidelis@live.com>
2021-08-11 22:00:48 +0200

    fix: load theme before render

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import 'prismjs/themes/prism-tomorrow.css'
 import CategoriesProvider from '../providers/CategoriesProvider'
 import SeriesProvider from '../providers/SeriesProvider'
@@ -13,6 +14,37 @@ const App: React.ComponentType<Props> = ({ Component, pageProps }) => {
   return (
     <CategoriesProvider>
       <SeriesProvider>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+const getPrefersDarkMode = () => {
+  if (typeof window !== 'undefined')
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+
+  return false
+}
+
+const getLocalStorageDarkMode = () => {
+  if (typeof window !== 'undefined')
+    return window?.localStorage.getItem('isDarkMode') === 'true'
+
+  return false
+}
+
+const isDark = getLocalStorageDarkMode() ?? getPrefersDarkMode() ?? false
+
+if (isDark) {
+  document.documentElement.classList.add('dark')
+  document.body.style.backgroundColor = '#070a0e'
+} else {
+  document.documentElement.classList.remove('dark')
+  document.body.style.backgroundColor = '#fff'
+}
+            `,
+            }}
+          />
+        </Head>
         <Component {...pageProps} />
       </SeriesProvider>
     </CategoriesProvider>
