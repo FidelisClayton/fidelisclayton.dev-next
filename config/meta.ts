39c78203d8c2137db56b2dd78d5bb6faf7d709cd
Clayton Fidelis <clayton.fidelis@live.com>
2021-08-12 15:34:47 +0200

    fix: tweak bio

--- config/meta.ts
+++ config/meta.ts
@@ -2,7 +2,7 @@ export default {
   siteTitle: 'Clayton Fidelis',
   url: 'https://fidelisclayton.dev',
   bio:
-    "I'm a front-end engineer, which loves JavaScript and functional programming. In this blog, I share my learnings and thoughts about software engineering, but mostly front-end development.",
+    "I'm a front-end engineer, who loves JavaScript and functional programming. In this blog, I share my learnings and thoughts about software engineering, but mostly front-end development.",
   twitterHandle: '@fidelisclayton',
   description:
     'Learn about Front-end, JavaScript, HTML, CSS, React, Elm, Tailwind, Styled Components, NextJS, NodeJS',
